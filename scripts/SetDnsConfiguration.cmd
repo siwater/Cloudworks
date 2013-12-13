37232a9ff26d6661b1c7076e2afe3876614cb02d
Simon Waterhouse <simon.waterhouse@citrix.com>
2013-12-13 17:36:14 +0000

    Modify cmd scripts to pushd/popd

--- scripts/SetDnsConfiguration.cmd
+++ scripts/SetDnsConfiguration.cmd
@@ -1,6 +1,7 @@
 @echo off
-cd %~dp0
+pushd %~dp0
 for %%a in ("%PSModulePath:;=";"%") do if %%a=="%~dp0" goto Exec
 set PSModulePath=%PsModulePath%;%~dp0
 :Exec
 powershell.exe -ExecutionPolicy Unrestricted .\%~nn0.ps1 %*
+popd
