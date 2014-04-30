81b26fdd233aa93283fb7dde687a5e1de2cc5d65
Simon Waterhouse <simon.waterhouse@citrix.com>
2014-04-30 13:37:39 +0100

    Remove obosolete experimental script

--- scripts/InstallRds.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-pushd %~dp0
-for %%a in ("%PSModulePath:;=";"%") do if %%a=="%~dp0" goto Exec
-set PSModulePath=%PsModulePath%;%~dp0
-:Exec
-powershell.exe -ExecutionPolicy Unrestricted .\%~nn0.ps1 %*
-popd
