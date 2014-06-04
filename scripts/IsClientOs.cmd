a8ad8cf47777b44c54a78ad0efa5b2a4d5fffbee
Simon Waterhouse <simon.waterhouse@citrix.com>
2014-06-04 14:34:32 +0100

    Utility to test for MS client OS (Windows 7/8 etc)

---
+++ scripts/IsClientOs.cmd
@@ -0,0 +1,8 @@
+@echo off
+pushd %~dp0
+for %%a in ("%PSModulePath:;=";"%") do if %%a=="%~dp0" goto Exec
+set PSModulePath=%PsModulePath%;%~dp0
+:Exec
+powershell.exe -ExecutionPolicy Unrestricted .\%~nn0.ps1 %*
+popd
+exit /b %ERRORLEVEL%
